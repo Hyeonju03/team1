85e58a1ed01da1899c55d79ccd35191b2351d5b6
ksj-s-j <ksjsi02000@naver.com>
2024-10-16 18:17:37 +0900

    프로젝트 파일 수정
    js ListLibrary 수정
    메신저 조직도 자동화 부서까지 완료

--- team1/frontend/src/HtmlFunctions/ListLibrary.js
+++ team1/frontend/src/HtmlFunctions/ListLibrary.js
@@ -1,23 +1,51 @@
+import React, {createElement} from "react";
+
 class ListLibrary {
-    static testData1 = ["배럭", "테란", "마린", "팩토리", "탱크", "벌처", "매딕", "파벳", "고스트","스타포트","레이스","드랍쉽"];
-    static testData2 = ["테란", "", "배럭", "테란", "팩토리", "팩토리", "배럭", "배럭", "배럭","테란","스타포트","스타포트"];
+    static testData1 = ["배럭", "테란", "마린", "팩토리", "탱크", "벌처", "매딕", "파벳", "고스트", "스타포트", "레이스", "드랍쉽"];
+    static testData2 = ["테란", "", "배럭", "테란", "팩토리", "팩토리", "배럭", "배럭", "배럭", "테란", "스타포트", "스타포트"];
     static testData3 = {};
+    static tagList = {};
     static comName = "스타";
     static list1 = "";
     static list2 = "";
-
+    static top = ""
     static WorkerList() {
-        const findFun = (topData, parent) => {
-            this.testData2.forEach((v1, i1) => {
-                if (topData === v1) {
-                    if (this.testData3[this.testData1[i1]] !== parent && parent !== "") {
-                        this.testData3[this.testData1[i1]] = parent + this.testData1[i1] + ",";
-                    } else {
-                        this.testData3[this.testData1[i1]] += this.testData1[i1] + ",";
+        // 절대 지우지 마시오.
+        // const navigation = (topData, parent) => {
+        //     this.testData2.forEach((v1, i1) => {
+        //         if (topData === v1) {
+        //             if (this.testData3[this.testData1[i1]] !== parent && parent !== "") {
+        //                 this.testData3[this.testData1[i1]] = parent + this.testData1[i1] + ",";
+        //             } else {
+        //                 this.testData3[this.testData1[i1]] += this.testData1[i1] + ",";
+        //             }
+        //             navigation(this.testData1[i1], this.testData3[this.testData1[i1]]);
+        //         }
+        //     });
+        // }
+        // 정대 지우지 마시오.
+
+        const reversePath = () => {
+            let htmlList = {};
+            this.top = ""
+            this.testData1.forEach((v1, i1) => {
+                const htmlTag = new DOMParser().parseFromString(`<li id=${v1} class=${this.testData2[i1]}>${v1}<ul class="list-disc pl-5"></ul></li>`, 'text/html').body.firstChild;
+                const id = htmlTag.id
+                htmlList[id] = htmlTag;
+            })
+            this.testData1.forEach((v1, i1) => {
+                this.testData1.forEach((v2, i2) => {
+                    if (htmlList[v1].id !== htmlList[v2].id) {
+                        if (htmlList[v1].id === htmlList[v2].className) {
+                            htmlList[v1].children[0].appendChild(htmlList[v2])
+                        }
                     }
-                    findFun(this.testData1[i1], this.testData3[this.testData1[i1]]);
+                });
+                if (this.testData2[i1] === "") {
+                    this.top = v1
                 }
-            });
+            })
+            return htmlList[this.top]
         }
 
         const listCheck = () => {
@@ -30,13 +58,15 @@ class ListLibrary {
                     this.testData1.forEach((v2, i2) => {
                         this.testData3[v2] = v1 + ",";
                     });
-                    this.testData3[v1] = ""; // 초기값 설정
-                    findFun(v1, this.testData3[v1]);
+                    // 절대 지우지 마시오.
+                    //this.testData3[v1] = "";
+                    // navigation(v1, this.testData3[v1]);
+                    // 절대 지우지 마시오.
                 }
             });
-
+            this.list1 = reversePath()
             console.log(this.testData3);
-            return <div dangerouslySetInnerHTML={{ __html: this.list1 }} />;
+            return <div dangerouslySetInnerHTML={{__html: this.list1.outerHTML}}/>;
         }
 
         return (
